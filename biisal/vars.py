5c06b557691392aa472b1e1f22ca9a6a6ddc0357
Athula1996 <110589162+Athula1996@users.noreply.github.com>
2024-03-26 10:31:31 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,8 +18,8 @@ class Var(object):
     name = str(getenv('name', 'Proton_file_to_link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001524622686'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001524622686'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '1001524622686'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '1001524622686'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -33,7 +33,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', 'https://u1copy-6e29fcad33e4.herokuapp.com:8080')) if not ON_HEROKU or getenv('FQDN', 'https://u1copy-6e29fcad33e4.herokuapp.com') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
