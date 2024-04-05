0a45da78de03c95f8277c66bd2ed2ac98ab1931c
Athula1996 <110589162+Athula1996@users.noreply.github.com>
2024-04-05 12:44:07 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -6,16 +6,16 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
-bisal_channel = "https://telegram.me/bisal_files"
-bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
+bot_name = "Proton File To Link"
+bisal_channel = "https://telegram.me/joinnowearn"
+bisal_grp = "https://telegram.me/joinnowearn"
 
 class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '10389378'))
     API_HASH = str(getenv('API_HASH', 'cdd5c820cb6abeecaef38e2bb8db4860'))
     BOT_TOKEN = str(getenv('BOT_TOKEN' , '6627128042:AAHWBTqCLeIMKSOLkE4D9xgrmnFGYY3iR-k'))
-    name = str(getenv('name', 'bisal_file2link_bot'))
+    name = str(getenv('name', '@Proton_file_to_link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001524622686'))
@@ -40,7 +40,7 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://reddead:deadred@cluster0.btdcf9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'joinnowearn')) 
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
-    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'joinnowearn'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001524622686")).split()))   
+    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1001524622686")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @joinnowearn ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
